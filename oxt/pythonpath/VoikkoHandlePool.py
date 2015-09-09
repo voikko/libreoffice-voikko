b62208b368858ee8f07ed32fe9fce4004c511c84
Harri Pitkänen <hatapitk@iki.fi>
2015-09-09 18:27:44 +0300

    getSupportedSpellingLanguages

--- oxt/pythonpath/VoikkoHandlePool.py
+++ oxt/pythonpath/VoikkoHandlePool.py
@@ -9,15 +9,33 @@
 # the GNU General Public License Version 3 or later (the "GPL"), in which
 # case the provisions of the GPL are applicable instead of those above.
 
+from libvoikko import Voikko
+
 class VoikkoHandlePool:
 
 	def __init__(self):
-		pass #TODO
+		self.__supportedSpellingLocales = ()
+		self.__installationPath = None
 
 	def getInstance():
 		if VoikkoHandlePool.instance is None:
 			VoikkoHandlePool.instance = VoikkoHandlePool()
 		return VoikkoHandlePool.instance
 	getInstance = staticmethod(getInstance)
+
+	def getInstallationPath(self):
+		return self.__installationPath
+
+	def __addLocale(self, locales, language):
+		pass # TODO
+
+	def getSupportedSpellingLocales(self):
+		# optimization: if we already have found some locales, don't search for more
+		if len(self.__supportedSpellingLocales) == 0:
+			languages = Voikko.listSupportedSpellingLanguages(self.getInstallationPath())
+			for lang in languages:
+				self.__addLocale(self.__supportedSpellingLocales, lang)
+		return self.__supportedSpellingLocales
+
 
 VoikkoHandlePool.instance = None
