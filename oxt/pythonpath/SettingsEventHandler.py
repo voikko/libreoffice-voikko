7295449ded877529a0c3007da3617dee555ee97b
Harri Pitkänen <hatapitk@iki.fi>
2015-09-20 13:36:32 +0300

    Implement callHandlerMethod

--- oxt/pythonpath/SettingsEventHandler.py
+++ oxt/pythonpath/SettingsEventHandler.py
@@ -10,11 +10,58 @@
 # case the provisions of the GPL are applicable instead of those above.
 
 import unohelper
+import logging
+from com.sun.star.lang import XServiceInfo, XInitialization, XServiceDisplayName
+from com.sun.star.awt import XContainerWindowEventHandler
+from com.sun.star.beans import UnknownPropertyException
+from PropertyManager import PropertyManager
 
-class SettingsEventHandler(unohelper.Base):
+class SettingsEventHandler(unohelper.Base, XServiceInfo, XContainerWindowEventHandler):
 
 	def __init__(self, ctx, *args):
 		print("SettingsEventHandler.__init__")
+
+	# From XServiceInfo
+	def getImplementationName(self):
+		return SettingsEventHandler.IMPLEMENTATION_NAME
+
+	# From XContainerWindowEventHandler
+	def callHandlerMethod(self, xWindow, eventObject, methodName):
+		if methodName != "external_event":
+			return False
+		if eventObject == "ok":
+			self.__saveOptionsFromWindowToRegistry(xWindow)
+			PropertyManager.getInstance().reloadVoikkoSettings()
+			return True
+		if eventObject == "back" or eventObject == "initialize":
+			self.__initOptionsWindowFromRegistry(xWindow)
+			return True
+		return False
+
+	def getSupportedMethodNames(self):
+		return ("external_event",)
+
+	def __initOptionsWindowFromRegistry(self, window):
+		logging.debug("initOptionsWindowFromRegistry()");
+		hyphWordPartsValue = False
+		hyphUnknownWordsValue = True
+		try:
+			hyphWordPartsValue = PropertyManager.getInstance().readFromRegistry("/org.puimula.ooovoikko.Config/hyphenator",  "hyphWordParts")
+			hyphUnknownWordsValue = PropertyManager.getInstance().readFromRegistry("/org.puimula.ooovoikko.Config/hyphenator", "hyphUnknownWords")
+		except UnknownPropertyException as e:
+			logging.exception("SettingsEventHandler: UnknownPropertyException", e)
+			return
+		logging.debug("hyphWordParts = " + str(hyphWordPartsValue))
+		hyphWordParts = window.getControl("hyphWordParts")
+
+		hyphWordPartsProps = hyphWordParts.getModel()
+		hyphWordPartsProps.setPropertyValue("State", 1 if hyphWordPartsValue else 0)
+
+		hyphUnknownWords = window.getControl("hyphUnknownWords")
+		hyphUnknownWordsProps = hyphUnknownWords.getModel()
+		hyphUnknownWordsProps.setPropertyValue("State", 1 if hyphUnknownWordsValue else 0)
+
+		self.__initVariantDropdown(window)
 
 SettingsEventHandler.IMPLEMENTATION_NAME = "org.puimula.ooovoikko.SettingsEventHandlerImplementation"
 SettingsEventHandler.SUPPORTED_SERVICE_NAMES = ("org.puimula.ooovoikko.SettingsEventHandlerService",)
